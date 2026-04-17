05b346e6b16b5fa5c934f7ffad823ac4a3efab50
cauasousa <llkauall055@gmail.com>
2026-04-17 11:14:32 -0300

    build

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# 1. Instalar Flutter
+# 1. Instalar Flutter se não existir
 if [ ! -d "flutter" ]; then
   git clone https://github.com/flutter/flutter.git -b stable
 fi
@@ -9,12 +9,14 @@ fi
 # 2. Configurar PATH
 export PATH="$PATH:$(pwd)/flutter/bin"
 
-# 3. Forçar o download dos artefatos de Web (O segredo para reconhecer a flag)
+# 3. Forçar o Flutter a se "reconhecer" como Web
 flutter config --enable-web
 flutter precache --web
 
-# 4. Baixar dependências do seu projeto
+# 4. Baixar dependências
 flutter pub get
 
-# 5. Build com a flag
-flutter build web --release --web-renderer canvaskit
+# 5. Build (Trocando a flag por uma alternativa caso a versão tenha mudado)
+# Se o erro 64 persistir aqui, o Flutter está em uma versão que usa 
+# apenas 'flutter build web' e assume o renderer automaticamente.
+flutter build web --release --wasm --web-renderer canvaskit
