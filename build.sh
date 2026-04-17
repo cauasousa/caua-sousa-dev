458ed82997914814fc673a9e2767693c1649e07b
cauasousa <llkauall055@gmail.com>
2026-04-17 11:05:21 -0300

    build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-# Instala o Flutter caso não exista (a Vercel usa ambientes Linux)
+# Interrompe o script se houver erro
+set -e
+
+# 1. Instala o Flutter caso não exista
 if [ ! -d "flutter" ]; then
   git clone https://github.com/flutter/flutter.git -b stable
 fi
-export PATH="$PATH:`pwd`/flutter/bin"
-flutter doctor
-flutter build web --release --web-renderer canvaskit
+
+# 2. Configura o PATH de forma absoluta
+export PATH="$PATH:$(pwd)/flutter/bin"
+
+# 3. Baixa as dependências do seu projeto (Essencial para não dar erro 64)
+flutter pub get
+
+# 4. Build com sintaxe corrigida
+flutter build web --release --web-renderer=canvaskit
